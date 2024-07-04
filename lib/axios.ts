7998e99c792773f1d71c4f127c46a1d94312d08e
john mwangi <modernacyber@gmail.com>
2024-07-04 15:41:32 +0300

    constants

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8000";
+import { BASE_URL } from "./constants"
 export default axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
